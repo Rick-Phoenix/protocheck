05947faeadda86ff08efe9d8f5be0e762e299401
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-11 21:45:08 +0200

    Improved logo size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,19 +12,16 @@
 </script>
 
 <div class="flex-col-center size-full gap-4">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <h1 class="text-center">Welcome to Tauri + Svelte</h1>
   <div class="flex-center gap-3">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
     <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+      <img src="/tauri.svg" class="size-24" alt="Tauri Logo" />
     </a>
     <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+      <img src="/svelte.svg" class="size-24" alt="SvelteKit Logo" />
     </a>
   </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+  <p>Click on the Tauri and SvelteKit logos to learn more.</p>
 
   <form class="flex-center" onsubmit={greet}>
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
