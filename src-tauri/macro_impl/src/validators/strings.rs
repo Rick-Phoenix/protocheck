e4e04c2e69005a1284f6522cb33803ead404ddb7
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-24 20:10:24 +0200

    Added string.len

--- src-tauri/macro_impl/src/validators/strings.rs
+++ src-tauri/macro_impl/src/validators/strings.rs
@@ -70,9 +70,159 @@ pub fn max_len(
     let violation = Violation {
       rule_id: Some("string.max_len".to_string()),
       message: Some(format!(
-        "{} cannot be longer than {} character{}",
+        "`{}` cannot be longer than {} character{}",
         field_context.field_data.proto_name.clone(),
         max_len,
+        plural_suffix
+      )),
+      for_key: Some(field_context.field_data.is_map_key),
+      field: Some(FieldPath { elements: elements }),
+      rule: Some(FieldPath {
+        elements: violation_elements,
+      }),
+    };
+    return Err(violation);
+  };
+  Ok(())
+}
+
+pub fn min_len(
+  field_context: FieldContext,
+  value: Option<&str>,
+  min_len: usize,
+) -> Result<(), Violation> {
+  let check = if value.is_some() {
+    let unwrapped_val = value.unwrap();
+    unwrapped_val.chars().count() >= min_len
+  } else {
+    return Ok(());
+  };
+
+  let plural_suffix = if min_len > 1 {
+    format!("s")
+  } else {
+    format!("")
+  };
+
+  if !check {
+    let mut elements = field_context.parent_elements.to_vec();
+    let current_elem = FieldPathElement {
+      field_type: Some(ProtoTypes::String.into()),
+      field_name: Some(field_context.field_data.proto_name.clone()),
+      key_type: field_context.field_data.key_type.map(|t| t as i32),
+      value_type: field_context.field_data.value_type.map(|t| t as i32),
+      field_number: Some(field_context.field_data.tag as i32),
+      subscript: field_context.subscript,
+    };
+
+    elements.push(current_elem);
+
+    let FieldData {
+      is_repeated_item,
+      is_map_key,
+      is_map_value,
+      ..
+    } = field_context.field_data;
+
+    let mut violation_elements =
+      get_base_violations_path(is_repeated_item, is_map_key, is_map_value);
+
+    violation_elements.extend(vec![
+      FieldPathElement {
+        field_name: Some("string".to_string()),
+        field_number: Some(14),
+        field_type: Some(ProtoTypes::Message as i32),
+        subscript: None,
+        key_type: None,
+        value_type: None,
+      },
+      FieldPathElement {
+        field_name: Some("min_len".to_string()),
+        field_number: Some(2),
+        field_type: Some(ProtoTypes::Uint64 as i32),
+        key_type: None,
+        value_type: None,
+        subscript: None,
+      },
+    ]);
+
+    let violation = Violation {
+      rule_id: Some("string.min_len".to_string()),
+      message: Some(format!(
+        "`{}` cannot be shorter than {} character{}",
+        field_context.field_data.proto_name.clone(),
+        min_len,
+        plural_suffix
+      )),
+      for_key: Some(field_context.field_data.is_map_key),
+      field: Some(FieldPath { elements: elements }),
+      rule: Some(FieldPath {
+        elements: violation_elements,
+      }),
+    };
+    return Err(violation);
+  };
+  Ok(())
+}
+
+pub fn len(field_context: FieldContext, value: Option<&str>, len: usize) -> Result<(), Violation> {
+  let check = if value.is_some() {
+    let unwrapped_val = value.unwrap();
+    unwrapped_val.chars().count() == len
+  } else {
+    return Ok(());
+  };
+
+  let plural_suffix = if len > 1 { format!("s") } else { format!("") };
+
+  if !check {
+    let mut elements = field_context.parent_elements.to_vec();
+    let current_elem = FieldPathElement {
+      field_type: Some(ProtoTypes::String.into()),
+      field_name: Some(field_context.field_data.proto_name.clone()),
+      key_type: field_context.field_data.key_type.map(|t| t as i32),
+      value_type: field_context.field_data.value_type.map(|t| t as i32),
+      field_number: Some(field_context.field_data.tag as i32),
+      subscript: field_context.subscript,
+    };
+
+    elements.push(current_elem);
+
+    let FieldData {
+      is_repeated_item,
+      is_map_key,
+      is_map_value,
+      ..
+    } = field_context.field_data;
+
+    let mut violation_elements =
+      get_base_violations_path(is_repeated_item, is_map_key, is_map_value);
+
+    violation_elements.extend(vec![
+      FieldPathElement {
+        field_name: Some("string".to_string()),
+        field_number: Some(14),
+        field_type: Some(ProtoTypes::Message as i32),
+        subscript: None,
+        key_type: None,
+        value_type: None,
+      },
+      FieldPathElement {
+        field_name: Some("len".to_string()),
+        field_number: Some(19),
+        field_type: Some(ProtoTypes::Uint64 as i32),
+        key_type: None,
+        value_type: None,
+        subscript: None,
+      },
+    ]);
+
+    let violation = Violation {
+      rule_id: Some("string.len".to_string()),
+      message: Some(format!(
+        "`{}` must be exactly {} character{} long",
+        field_context.field_data.proto_name.clone(),
+        len,
         plural_suffix
       )),
       for_key: Some(field_context.field_data.is_map_key),
--- src-tauri/pr_macro/src/validator/string_rules.rs
+++ src-tauri/pr_macro/src/validator/string_rules.rs
@@ -13,8 +13,37 @@ pub fn get_string_rules(
 ) -> Result<Vec<ValidatorCallTemplate>, Box<dyn std::error::Error>> {
   let mut templates: Vec<ValidatorCallTemplate> = Vec::new();
 
+  let mut min_len: Option<usize> = None;
+  let mut max_len: Option<usize> = None;
+  let mut len: Option<usize> = None;
+
+  if string_rules.len.is_some() {
+    let len_value = string_rules.len.unwrap() as usize;
+    len = Some(len_value);
+
+    templates.push(ValidatorCallTemplate {
+      validator_path: Some(quote! { macro_impl::validators::strings::len }),
+      target_value_tokens: Some(len_value.into_token_stream()),
+      field_data: field_data.clone(),
+      kind: GeneratedCodeKind::FieldRule,
+    });
+  }
+
+  if string_rules.min_len.is_some() {
+    let min_len_value = string_rules.min_len.unwrap() as usize;
+    min_len = Some(min_len_value);
+
+    templates.push(ValidatorCallTemplate {
+      validator_path: Some(quote! { macro_impl::validators::strings::min_len }),
+      target_value_tokens: Some(min_len_value.into_token_stream()),
+      field_data: field_data.clone(),
+      kind: GeneratedCodeKind::FieldRule,
+    });
+  }
+
   if string_rules.max_len.is_some() {
     let max_len_value = string_rules.max_len.unwrap() as usize;
+    max_len = Some(max_len_value);
 
     templates.push(ValidatorCallTemplate {
       validator_path: Some(quote! { macro_impl::validators::strings::max_len }),
@@ -22,6 +51,22 @@ pub fn get_string_rules(
       field_data: field_data.clone(),
       kind: GeneratedCodeKind::FieldRule,
     });
+  }
+
+  if len.is_some() && (min_len.is_some() || max_len.is_some()) {
+    return Err(Box::new(syn::Error::new(
+      Span::call_site(),
+      "string.len cannot be used with string.max_len or string.min_len",
+    )));
+  }
+
+  if min_len.is_some() && max_len.is_some() {
+    if min_len.unwrap() > max_len.unwrap() {
+      return Err(Box::new(syn::Error::new(
+        Span::call_site(),
+        "string.min_len cannot be larger than string.max_len",
+      )));
+    }
   }
 
   Ok(templates)
