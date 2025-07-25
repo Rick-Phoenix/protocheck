788b1dfce4ab997fbe4634b74bce9172e347dcff
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-25 15:12:52 +0200

    Encapsulated build function

--- protocheck/src/lib.rs
+++ protocheck/src/lib.rs
@@ -1,3 +1,58 @@
 pub use proto_types as types;
 pub use protocheck_core::{field_data, validators};
 pub use protocheck_proc_macros as macros;
+
+pub mod build {
+  use std::{
+    env,
+    error::Error,
+    io::Read,
+    path::{Path, PathBuf},
+  };
+
+  use prost_build::Config;
+  use prost_reflect::{prost::Message, prost_types::FileDescriptorSet};
+
+  pub fn compile_protos_with_validators(
+    config: &mut Config,
+    proto_files: &[impl AsRef<Path>],
+    proto_include_paths: &[impl AsRef<Path>],
+    app_package_prefix: &str,
+  ) -> Result<(), Box<dyn Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let temp_descriptor_path = out_dir.join("temp_file_descriptor_set_for_protocheck.bin");
+    {
+      let mut temp_config = prost_build::Config::new();
+      temp_config.file_descriptor_set_path(temp_descriptor_path.clone());
+      temp_config.compile_protos(proto_files, proto_include_paths)?;
+    }
+
+    let mut fds_file = std::fs::File::open(&temp_descriptor_path)?;
+    let mut fds_bytes = Vec::new();
+    fds_file.read_to_end(&mut fds_bytes)?;
+    let fds = FileDescriptorSet::decode(fds_bytes.as_slice())?;
+    let pool = prost_reflect::DescriptorPool::from_file_descriptor_set(fds)?;
+
+    let mut full_message_names = Vec::new();
+    for message_desc in pool.all_messages() {
+      if message_desc.full_name().starts_with(app_package_prefix) {
+        full_message_names.push(message_desc.full_name().to_string());
+      }
+    }
+
+    for full_name in full_message_names {
+      let attribute_str = format!(
+        r#"#[protocheck::macros::protobuf_validate("{}")]"#,
+        full_name
+      );
+      config.message_attribute(full_name, &attribute_str);
+    }
+
+    config.compile_protos(proto_files, proto_include_paths)?;
+
+    std::fs::remove_file(&temp_descriptor_path)?;
+
+    Ok(())
+  }
+}
--- tests/build.rs
+++ tests/build.rs
@@ -1,7 +1,5 @@
-use std::io::Read;
-
 use prost_build::Config;
-use prost_reflect::{prost::Message, prost_types::FileDescriptorSet};
+use protocheck::build::compile_protos_with_validators;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!("cargo:rerun-if-changed=proto/");
@@ -10,8 +8,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
   let final_descriptor_path = out_dir.join("file_descriptor_set.bin");
 
-  let temp_descriptor_path = out_dir.join("temp_file_descriptor_set.bin");
-
   let proto_include_paths = &["proto", "proto_deps"];
 
   let proto_files = &[
@@ -19,25 +15,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     "proto_deps/buf/validate/validate.proto",
     // "proto/myapp/v1/post.proto",
   ];
-
-  {
-    let mut temp_config = prost_build::Config::new();
-    temp_config.file_descriptor_set_path(temp_descriptor_path.clone());
-    temp_config.compile_protos(proto_files, proto_include_paths)?;
-  }
-
-  let mut fds_file = std::fs::File::open(&temp_descriptor_path)?;
-  let mut fds_bytes = Vec::new();
-  fds_file.read_to_end(&mut fds_bytes)?;
-  let fds = FileDescriptorSet::decode(fds_bytes.as_slice())?;
-  let pool = prost_reflect::DescriptorPool::from_file_descriptor_set(fds)?;
-
-  let mut full_message_names = Vec::new();
-  for message_desc in pool.all_messages() {
-    if message_desc.full_name().starts_with("myapp.v1") {
-      full_message_names.push(message_desc.full_name().to_string());
-    }
-  }
 
   let mut config = Config::new();
   config
@@ -48,17 +25,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     .compile_well_known_types()
     .out_dir(out_dir.clone());
 
-  for full_name in full_message_names {
-    let attribute_str = format!(
-      r#"#[protocheck::macros::protobuf_validate("{}")]"#,
-      full_name
-    );
-    config.message_attribute(full_name, &attribute_str);
-  }
-
-  config.compile_protos(proto_files, proto_include_paths)?;
-
-  std::fs::remove_file(&temp_descriptor_path)?;
+  compile_protos_with_validators(&mut config, proto_files, proto_include_paths, "myapp.v1")?;
 
   println!(
     "cargo:rustc-env=PROTO_DESCRIPTOR_SET={}",
