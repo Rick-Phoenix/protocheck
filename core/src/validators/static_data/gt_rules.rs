c97df1361398678abee909d138bc5657c29acfd3
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-29 12:48:57 +0200

    Added gt and lt rules

---
+++ core/src/validators/static_data/gt_rules.rs
@@ -0,0 +1,315 @@
+use std::sync::LazyLock;
+
+use crate::{protovalidate::FieldPathElement, ProtoType};
+
+pub(crate) fn get_gt_rule_path(kind: &ProtoType) -> (&str, Vec<FieldPathElement>) {
+  match kind {
+    ProtoType::Float => ("float", FLOAT_GT_VIOLATION.clone()),
+    ProtoType::Double => ("double", DOUBLE_GT_VIOLATION.clone()),
+    ProtoType::Int32 => ("int32", INT32_GT_VIOLATION.clone()),
+    ProtoType::Int64 => ("int64", INT64_GT_VIOLATION.clone()),
+    ProtoType::Uint32 => ("uint32", UINT32_GT_VIOLATION.clone()),
+    ProtoType::Uint64 => ("uint64", UINT64_GT_VIOLATION.clone()),
+    ProtoType::Sint32 => ("sint32", SINT32_GT_VIOLATION.clone()),
+    ProtoType::Sint64 => ("sint64", SINT64_GT_VIOLATION.clone()),
+    ProtoType::Fixed32 => ("fixed32", FIXED32_GT_VIOLATION.clone()),
+    ProtoType::Fixed64 => ("fixed64", FIXED64_GT_VIOLATION.clone()),
+    ProtoType::Sfixed32 => ("sfixed32", SFIXED32_GT_VIOLATION.clone()),
+    ProtoType::Sfixed64 => ("sfixed64", SFIXED64_GT_VIOLATION.clone()),
+    _ => ("", vec![]),
+  }
+}
+
+static FLOAT_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("float".to_string()),
+      field_number: Some(1),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Float as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static DOUBLE_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("double".to_string()),
+      field_number: Some(2),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Double as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static INT32_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("int32".to_string()),
+      field_number: Some(3),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Int32 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static INT64_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("int64".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Int64 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static UINT32_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("uint32".to_string()),
+      field_number: Some(5),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Uint32 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static UINT64_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("uint64".to_string()),
+      field_number: Some(6),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Uint64 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static SINT32_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("sint32".to_string()),
+      field_number: Some(7),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Sint32 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static SINT64_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("sint64".to_string()),
+      field_number: Some(8),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Sint64 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static FIXED32_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("fixed32".to_string()),
+      field_number: Some(9),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Fixed32 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static FIXED64_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("fixed64".to_string()),
+      field_number: Some(10),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Fixed64 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static SFIXED32_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("sfixed32".to_string()),
+      field_number: Some(11),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Sfixed32 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static SFIXED64_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("sfixed64".to_string()),
+      field_number: Some(12),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(4),
+      field_type: Some(ProtoType::Sfixed64 as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static DURATION_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("duration".to_string()),
+      field_number: Some(21),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(5),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
+
+static TIMESTAMP_GT_VIOLATION: LazyLock<Vec<FieldPathElement>> = LazyLock::new(|| {
+  vec![
+    FieldPathElement {
+      field_name: Some("timestamp".to_string()),
+      field_number: Some(22),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+    FieldPathElement {
+      field_name: Some("gt".to_string()),
+      field_number: Some(5),
+      field_type: Some(ProtoType::Message as i32),
+      subscript: None,
+      key_type: None,
+      value_type: None,
+    },
+  ]
+});
--- core/src/validators/static_data/mod.rs
+++ core/src/validators/static_data/mod.rs
@@ -1,3 +1,7 @@
 pub mod const_rules;
+pub mod gt_rules;
+pub mod gte_rules;
 pub mod in_rules;
+pub mod lt_rules;
+pub mod lte_rules;
 pub mod not_in_rules;
