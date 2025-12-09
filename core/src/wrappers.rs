65d3736e5bcfe0e2aaaf793cbcf916a4f0447eff
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-12-09 16:24:34 +0100

    feat: added wrappers for integer types

--- core/src/wrappers.rs
+++ core/src/wrappers.rs
@@ -1,12 +1,24 @@
 macro_rules! impl_wrapper {
   ($name:ident, $target:ty) => {
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-    pub struct $name($target);
+    pub struct $name(pub $target);
 
     impl std::ops::Deref for $name {
       type Target = $target;
       fn deref(&self) -> &Self::Target {
         &self.0
+      }
+    }
+
+    impl std::cmp::PartialEq<$target> for $name {
+      fn eq(&self, other: &$target) -> bool {
+        self.0 == *other
+      }
+    }
+
+    impl From<$name> for $target {
+      fn from(value: $name) -> $target {
+        value.0
       }
     }
   };
