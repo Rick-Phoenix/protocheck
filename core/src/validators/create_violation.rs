2966cac9c8bd608106efee3c1c74b853dbd516c9
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-12-17 18:39:53 +0100

    perf: optimized violation creation

--- core/src/validators/create_violation.rs
+++ core/src/validators/create_violation.rs
@@ -1,34 +1,45 @@
 use super::*;
 use crate::protovalidate::{MAP_KEY_VIOLATION, MAP_VALUE_VIOLATION, REPEATED_ITEM_VIOLATION};
 
-fn create_violation_core(
+pub(crate) fn create_violation_core(
   custom_rule_id: Option<&str>,
-  field_context: &FieldContext,
+  field_context: Option<&FieldContext>,
+  parent_elements: &[FieldPathElement],
   violation_data: &ViolationData,
   error_message: &str,
-  parent_elements: &[FieldPathElement],
 ) -> Violation {
-  let mut field_elements = parent_elements.to_vec();
-
-  let current_elem = FieldPathElement {
-    field_type: Some(field_context.field_type as i32),
-    field_name: Some(field_context.proto_name.to_string()),
-    key_type: field_context.key_type.map(|t| t as i32),
-    value_type: field_context.value_type.map(|t| t as i32),
-    field_number: Some(field_context.tag),
-    subscript: field_context.subscript.clone(),
-  };
-
-  field_elements.push(current_elem);
-
+  let mut field_elements: Option<Vec<FieldPathElement>> = None;
   let mut rule_elements: Vec<FieldPathElement> = Vec::new();
+  let mut is_for_key = false;
 
-  match &field_context.field_kind {
-    FieldKind::MapKey => rule_elements.extend(MAP_KEY_VIOLATION.elements.to_vec()),
-    FieldKind::MapValue => rule_elements.extend(MAP_VALUE_VIOLATION.elements.to_vec()),
-    FieldKind::RepeatedItem => rule_elements.extend(REPEATED_ITEM_VIOLATION.elements.to_vec()),
-    _ => {}
-  };
+  // In case of a top level message with CEL violations applied to the message
+  // as a whole, there would be no field path
+  if let Some(field_context) = field_context {
+    let elements = field_elements.get_or_insert_default();
+
+    elements.extend(parent_elements.iter().cloned());
+
+    let current_elem = FieldPathElement {
+      field_type: Some(field_context.field_type as i32),
+      field_name: Some(field_context.proto_name.to_string()),
+      key_type: field_context.key_type.map(|t| t as i32),
+      value_type: field_context.value_type.map(|t| t as i32),
+      field_number: Some(field_context.tag),
+      subscript: field_context.subscript.clone(),
+    };
+
+    elements.push(current_elem);
+
+    match &field_context.field_kind {
+      FieldKind::MapKey => {
+        is_for_key = true;
+        rule_elements.extend(MAP_KEY_VIOLATION.elements.to_vec());
+      }
+      FieldKind::MapValue => rule_elements.extend(MAP_VALUE_VIOLATION.elements.to_vec()),
+      FieldKind::RepeatedItem => rule_elements.extend(REPEATED_ITEM_VIOLATION.elements.to_vec()),
+      _ => {}
+    };
+  }
 
   rule_elements.extend(violation_data.elements.to_vec());
 
@@ -37,13 +48,8 @@ fn create_violation_core(
       custom_rule_id.map_or_else(|| violation_data.name.to_string(), |id| id.to_string()),
     ),
     message: Some(error_message.to_string()),
-    for_key: field_context
-      .field_kind
-      .is_map_key()
-      .then_some(true),
-    field: Some(FieldPath {
-      elements: field_elements,
-    }),
+    for_key: Some(is_for_key),
+    field: field_elements.map(|elements| FieldPath { elements }),
     rule: Some(FieldPath {
       elements: rule_elements,
     }),
@@ -58,10 +64,10 @@ pub(crate) fn create_violation(
 ) -> Violation {
   create_violation_core(
     None,
-    field_context,
+    Some(field_context),
+    parent_elements,
     violation_data,
     error_message,
-    parent_elements,
   )
 }
 
@@ -74,9 +80,9 @@ pub(crate) fn create_violation_with_custom_id(
 ) -> Violation {
   create_violation_core(
     Some(rule_id),
-    field_context,
+    Some(field_context),
+    parent_elements,
     violation_data,
     error_message,
-    parent_elements,
   )
 }
