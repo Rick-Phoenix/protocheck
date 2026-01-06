6c27fce6e4e36d5ef44f9e014b24c9914bd38b02
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-01-06 22:58:46 +0100

    feat(types): added to/from impls for Subscript

--- proto_types/src/protovalidate/mod.rs
+++ proto_types/src/protovalidate/mod.rs
@@ -9,6 +9,54 @@ use std::fmt::{self, Display};
 
 pub use violations::*;
 
+impl From<usize> for Subscript {
+  fn from(value: usize) -> Self {
+    Self::Index(value as u64)
+  }
+}
+
+impl From<i64> for Subscript {
+  fn from(value: i64) -> Self {
+    Self::IntKey(value)
+  }
+}
+
+impl From<i32> for Subscript {
+  fn from(value: i32) -> Self {
+    Self::IntKey(value.into())
+  }
+}
+
+impl From<u64> for Subscript {
+  fn from(value: u64) -> Self {
+    Self::UintKey(value)
+  }
+}
+
+impl From<u32> for Subscript {
+  fn from(value: u32) -> Self {
+    Self::UintKey(value.into())
+  }
+}
+
+impl From<bool> for Subscript {
+  fn from(value: bool) -> Self {
+    Self::BoolKey(value)
+  }
+}
+
+impl From<String> for Subscript {
+  fn from(value: String) -> Self {
+    Self::StringKey(value)
+  }
+}
+
+impl From<&str> for Subscript {
+  fn from(value: &str) -> Self {
+    Self::StringKey(value.to_string())
+  }
+}
+
 impl Display for Subscript {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
